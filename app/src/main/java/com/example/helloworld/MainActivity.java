e4b9bc20f5e0cd6fe9903c4fe5f1fd0ab7d1d576
smolev.max@gmail.com <smolev.max@gmail.com>
2016-09-05 22:24:46 +0400

    Я добавил кнопку Выход

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -1,13 +1,77 @@
 package com.example.helloworld;
+import android.app.Activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.AlertDialog;
+
+import android.app.Dialog;
+
+import android.content.DialogInterface;
+
+import android.content.DialogInterface.OnClickListener;
+
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import android.view.View;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    final int DIALOG_EXIT = 1;
+
+    /**
+     * Called when the activity is first created.
+     */
+
+    public void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void onclick(View v) {
+
+// вызываем диалог
+
+        showDialog(DIALOG_EXIT);
+
+    }
+
+    protected Dialog onCreateDialog(int id) {
+
+        if (id == DIALOG_EXIT) {
+
+            AlertDialog.Builder adb = new AlertDialog.Builder(this);
+            adb.setTitle(R.string.exit);
+            adb.setMessage(R.string.save_data);
+            adb.setIcon(android.R.drawable.ic_dialog_info);
+            adb.setPositiveButton(R.string.yes, myClickListener);
+            adb.setNegativeButton(R.string.no, myClickListener);
+            adb.setNeutralButton(R.string.cancel, myClickListener);
+            return adb.create();
+        }
+        return super.onCreateDialog(id);
+    }
+
+    OnClickListener myClickListener = new OnClickListener() {
+        public void onClick(DialogInterface dialog, int which) {
+            switch (which) {
+                case Dialog.BUTTON_POSITIVE:
+                    saveData();
+                    finish();
+                    break;
+                case Dialog.BUTTON_NEGATIVE:
+                    finish();
+                    break;
+                case Dialog.BUTTON_NEUTRAL:
+                    break;
+            }
+        }
+    };
+
+    void saveData() {
+        Toast.makeText(this, R.string.saved, Toast.LENGTH_SHORT).show();
     }
 }
